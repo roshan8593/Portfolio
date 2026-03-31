88a9140c5d78ce0e06c31f28825002cf925117c5
roshan <roshanarman149209@gmail.com>
2026-03-31 20:52:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import About from './components/about/about'
+import About from './components/about/About'
 import Footer from './components/footer/footer'
 import Education from './components/education/education'
 import Skills from './components/skills/skiils'
